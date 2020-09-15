553c45f3f539c76bcf71cffd9a490f859a50ba67
samuel <wushuang1227@gmail.com>
2020-09-15 11:11:38 +0800

    feat: spread cells

--- mineSweeper/src/lib/MineSweeper.js
+++ mineSweeper/src/lib/MineSweeper.js
@@ -4,7 +4,21 @@ const PREDEFINED_MAP = {
   MEDIUM: [16, 16, 40],
   LARGE: [16, 30, 99]
 }
+// 地雷方块值
 const MINE = 9
+// 安全方块值
+const ZERO = 0
+// 方块周围八个方块的偏移值
+const AROUND_OFFSET = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1]
+]
+// 游戏状态
+const SWEEPER_STATUS = {
+  ALIVE: 'ALIVE',
+  DEAD: 'DEAD'
+}
 
 function getSurroundingCellsPosition(ri, ci, rn, cn) {
   const pr = ri - 1
@@ -20,12 +34,31 @@ function getSurroundingCellsPosition(ri, ci, rn, cn) {
 
 export default class MineSweeper {
   constructor(mapSize) {
-    this._map = this.generateMap(PREDEFINED_MAP[mapSize.toUpperCase()])
-    this.board = this.generateBoard(this._map)
+    console.log(mapSize)
+    this.status = SWEEPER_STATUS.ALIVE
+    this._map = this._generateMap(PREDEFINED_MAP[mapSize.toUpperCase()])
+    this.board = this._generateBoard(this._map)
+  }
+
+  // 挖开指定位置方块
+  dig(rowIndex, cellIndex) {
+    // 如果已经死亡，点击无效
+    if (this.status === SWEEPER_STATUS.DEAD) {
+      return
+    }
+    const target = this.board[rowIndex][cellIndex]
+    target.digged = true
+    // 如果挖开雷，game over
+    if (target.value === MINE) {
+      return this._kill()
+    } else if (target.value === ZERO) {
+      // 如果挖开的方块是0，自动挖开周围的方块
+      this._spread(rowIndex, cellIndex)
+    }
   }
 
   // 根据地图尺寸生成随机地图
-  generateMap(mapDef) {
+  _generateMap(mapDef) {
     const [rowNum, cellNum, mineNum] = mapDef
     const total = rowNum * cellNum
     // 生成一维数组，前m个为雷
@@ -66,13 +99,47 @@ export default class MineSweeper {
   }
 
   // 根据地图生成游戏棋盘
-  generateBoard(map) {
-    return map.map(row => row.map(cell => ({
-      value: cell,
+  _generateBoard(map) {
+    return map.map((row, rowIndex) =>
+      row.map((cell, cellIndex) => this._cellFactory(rowIndex, cellIndex, cell)))
+  }
+
+  _cellFactory(rowIndex, cellIndex, value) {
+    return {
+      // 保存方块的值
+      value,
+      // 标记方块是否被插上红旗
       flagged: false,
+      // 标记方块是否被用户标上问号
       tagged: false,
-      opened: false
-    })))
+      // 标记方块是否被挖开
+      digged: false,
+      // 记录方块的行数
+      rowIndex,
+      // 记录方块的列数
+      cellIndex
+    }
+  }
+
+  // 根据给定位置方块挖开周围所有可自动挖开方块
+  _spread(rowIndex, cellIndex) {
+    const target = this.board[rowIndex][cellIndex]
+    if (target.value !== ZERO) return
+    AROUND_OFFSET.forEach(offset => {
+      const row = rowIndex + offset[0]
+      const cell = cellIndex + offset[1]
+      if (this.board[row] &&
+          this.board[row][cell] &&
+          this.board[row][cell].digged === false) {
+        this.board[row][cell].digged = true
+        this._spread(row, cell)
+      }
+    })
+  }
+
+  // game over
+  _kill() {
+    this.status = SWEEPER_STATUS.DEAD
   }
 
   // Fisher–Yates shuffle
